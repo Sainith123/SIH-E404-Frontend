2058f442da3d93153d880f9765086b355588af37
Sainith123 <yamsanisainith@gmail.com>
2024-09-09 11:51:47 +0530

    added default themes

---
+++ src/pages/theme.js
@@ -0,0 +1,13 @@
+// theme.js
+export default {
+    colors: {
+      primary: "#0070f3",
+      secondary: "#6d32ec",
+      background: "#f5f5f5",
+    },
+    fonts: {
+      base: "Arial, sans-serif",
+      heading: "Helvetica, sans-serif",
+    },
+  };
+  
